0e6adf98503eaea5deef30fa9ef2723284606ea9
Robin Bailey <rorgar1@yahoo.com>
2019-08-22 10:44:13 -0500

    program working with one error

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -9,33 +9,56 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
-@RequestMapping("cheese")
+@RequestMapping(value = "cheese")
 public class CheeseController {
 
     static HashMap<String, String> cheeses = new HashMap<>();
 
-// request path: /cheese
+// request path: cheese/
     @RequestMapping(value = "")
     public String index(Model model) {
 
         model.addAttribute("cheeses", cheeses);
-        model.addAttribute("title", "My Cheese");
+        model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
 
+    //request path cheese/add
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
+
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
+
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String description) {
+
         cheeses.put(cheeseName, description);
 
-        // Redirect to /cheese
+            //redirect to /cheese
+        return "redirect:";
+    }
+
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String  processRemoveCheeseForm(Model model) {
+        model.addAttribute("title", "Remove Cheese");
+        model.addAttribute("cheeses", this.cheeses);
+        return "cheese/remove";
+    }
+
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheeses) {
+        for (String cheese : cheeses) {
+            this.cheeses.remove(cheese);
+        }
+
         return "redirect:";
     }
 }
