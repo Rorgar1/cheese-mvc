f62b7c9941013f8f4d1a7c1ff1f7cddd3083dd71
Robin Bailey <rorgar1@yahoo.com>
2019-08-29 12:47:00 -0500

    user signup complete and greets user by name

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -2,34 +2,46 @@ package org.launchcode.cheesemvc.controllers;
 
 
 import org.launchcode.cheesemvc.models.User;
+import org.launchcode.cheesemvc.models.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("user")
 
 public class UserController {
 
-    @RequestMapping(value="add", method = RequestMethod.GET)
+    @RequestMapping(value="/add")
     public String add(Model model) {
         model.addAttribute("title", "Add User");
-        model.addAttribute("user", new User());
-        return "user/add/";
+        model.addAttribute(new User());
+        return "user/add";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify_password) {
-       model.addAttribute("user", user);
-       if(user.getPassword().equals(verify_password)) {
-           return "user/index";
-       }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String processAddUser(@ModelAttribute @Valid User user, Errors errors, Model model) {
 
-        model.addAttribute("error_message", "The passwords do not match. Please try again ");
-        return "user/add";
+       if(errors.hasErrors()) {
+           model.addAttribute("title", "Add User");
+           model.addAttribute("user", user);
+           return "user/add";
+       }
+       UserData.addUser(user);
+       return "redirect:";
         }
+    @RequestMapping(value = "")
+    public static String index(Model model) {
+        List<User> users = UserData.getUsers();
+        model.addAttribute("users", users);
+        return "user/index";
+    }
     }
 
 
--- src/main/java/org/launchcode/cheesemvc/models/User.java
+++ src/main/java/org/launchcode/cheesemvc/models/User.java
@@ -1,11 +1,40 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 3, max = 15)
     private String password;
 
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
+
+    private static int nextId = 0;
+
     public User() {
+        updateNextId();
+        this.id = nextId;
+    }
+
+    private static void updateNextId() {
+        nextId = nextId++;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public User(String username, String email, String password) {
@@ -38,4 +67,19 @@ public class User {
         this.password = password;
     }
 
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public User setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+        return this;
+    }
+
+    private void checkPassword() {
+        if (password != null && verifyPassword != null && !verifyPassword.equals(password)) {
+            verifyPassword = null;
+        }
+    }
 }
